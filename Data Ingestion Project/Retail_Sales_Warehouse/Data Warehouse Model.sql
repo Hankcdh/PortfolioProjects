958647d2e81b1c32fc919c285a100dabd7e358e5
Hank <hankcdh@yahoo.com.au>
2024-01-12 20:20:38 +1100

    add table constraints

--- Data Ingestion Project/Retail_Sales_Warehouse/Data Warehouse Model.sql
+++ Data Ingestion Project/Retail_Sales_Warehouse/Data Warehouse Model.sql
@@ -1,73 +1,54 @@
-
-
---Drop Table If Exist
+-- Drop Tables If They Exist
 DROP TABLE IF EXISTS RetailSalesFacts;
 DROP TABLE IF EXISTS ProductDim;
 DROP TABLE IF EXISTS DateDim;
 DROP TABLE IF EXISTS CashierDim;
 DROP TABLE IF EXISTS date_sequence;
- 
 
-
--- Retail Sales Transaction Fact Tables 
+-- Retail Sales Transaction Fact Table
 CREATE TABLE RetailSalesFacts(
-    Date_Key INT,
-    Product_key VARCHAR(10),
-    Store_Key VARCHAR(10),
-    Promotion_key VARCHAR (10),
-    Cashier_Key VARCHAR (10),
-    Payment_Method_key VARCHAR (10),
-    POS_Transaction VARCHAR(10),
-    Sales_Quantity float,
-    Regular_Unit_Price float,
-    Discount_Unit_Price float,
-    Net_Unit_Price float,
-    Extended_Discount_Dollar_Amount float,
-    Extended_Sales_Dollar_Amount float,
-    Extended_Cost_Amount float, 
-    Extended_Gross_Profit_Dollar_Amount float
-    
-    
+    Date_Key INT NOT NULL,
+    Product_key INT NOT NULL ,
+    Cashier_Key INT NOT NULL,
+    POS_Transaction VARCHAR(10) PRIMARY KEY,
+    Sales_Quantity FLOAT DEFAULT 0.0,
+    Regular_Unit_Price FLOAT DEFAULT 0.0,
+    CONSTRAINT valid_Sales_Quantity CHECK (Sales_Quantity > 0)
 );
 
+-- Product Dimension Table
 CREATE TABLE ProductDim(
-    Product_key INT,
-    SKU_Numer VARCHAR(10),
+    Product_key SERIAL PRIMARY KEY,
+    SKU_Number VARCHAR(10),
     Product_Description VARCHAR(20),
     Brand_Description VARCHAR(20)
 );
 
-
+-- Cashier Dimension Table
 CREATE TABLE CashierDim(
-    Cashier_Key INT,
+    Cashier_Key SERIAL PRIMARY KEY,
     Cashier_Employee_ID VARCHAR(10),
     Cashier_Name VARCHAR(10)
-    
 );
 
-
+-- Date Dimension Table
 CREATE TABLE DateDim(
-    Date_Key INT,
-    Actual_Date Date,
+    Date_Key INT PRIMARY KEY,
+    Actual_Date DATE,
     Day_Of_Week VARCHAR(5),
-    Calender_Month VARCHAR(5),
-    Calenger_Year VARCHAR(5)
+    Calendar_Month VARCHAR(5),
+    Calendar_Year VARCHAR(5)
 );
-
 
 -- Create Data for DateDim
+CREATE TABLE date_sequence (date DATE NOT NULL);
 
-CREATE TABLE date_sequence (date date NOT NULL);
+-- Populate date_sequence with 30 days starting from '2023-05-01'
+INSERT INTO date_sequence(date)
+SELECT '2023-05-01'::DATE + SEQUENCE.number AS date
+FROM GENERATE_SERIES(0, 30) AS SEQUENCE (number);
 
-INSERT INTO
-  date_sequence(date)
-  
-SELECT
-  '2023-05-01'::DATE + SEQUENCE.number AS date
-FROM
-  GENERATE_SERIES(0, 30) AS SEQUENCE (number);
-
-
+-- Populate DateDim with relevant information from date_sequence
 INSERT INTO DateDim
 SELECT   
     TO_CHAR(date, 'yyyymmdd')::INT AS date_dim_id,   
@@ -77,3 +58,9 @@ SELECT
     EXTRACT(YEAR FROM date) AS year_actual
 FROM date_sequence   
 ORDER BY date ASC;
+
+-- Define Foreign Key Constraints
+ALTER TABLE RetailSalesFacts 
+ADD CONSTRAINT FK_RetailSalesFacts_ProductDim FOREIGN KEY (Product_key) REFERENCES ProductDim (Product_key),
+ADD CONSTRAINT FK_RetailSalesFacts_DateDim FOREIGN KEY (Date_Key) REFERENCES DateDim (Date_Key),
+ADD CONSTRAINT FK_RetailSalesFacts_CashierDim FOREIGN KEY (Cashier_Key) REFERENCES CashierDim (Cashier_Key);
